b724777e562218aff684411832ab4bbcd7c1ff8a
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-06-29 02:44:10 +0000

    update

--- .vscode/test.js
+++ .vscode/test.js
@@ -8,11 +8,11 @@ test("1 euro es igual a 1.2 dolares", function(){
 
 test("1 dolar es igual 127.9 yenes ", function(){
     const { fromDollarToYen} = require('./app.js')
-    expect(fromDollarToYen(4.2)).toBe(537.18); 
+    expect(fromDollarToYen(4.2)).toBeCloseTo(537.18); 
 })
 
 
 test("1 yen es igual 0.8 libras", function(){
     const {fromYenToPound  } = require('./app.js')
-    expect(fromYenToPound (537.18)).toBe(429.744); 
+    expect(fromYenToPound (537.18)).toBeCloseTo(429.744); 
 })
