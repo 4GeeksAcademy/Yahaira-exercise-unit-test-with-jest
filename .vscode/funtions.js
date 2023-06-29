020500787658c677715277df56db0476c5182c1e
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-06-29 02:00:27 +0000

    up date 4

--- .vscode/funtions.js
+++ .vscode/funtions.js
@@ -6,21 +6,16 @@ let oneEuroIs = {
 
 
 const fromEuroToDollar = function(valueInEuro){
-    
     let valueInDollar = valueInEuro * 1.2;
-   
     return valueInDollar;
    }
    const fromDollarToYen = function(valueInDollar){
-    
     let valueInYen = valueInDollar * 127.9;
     return valueInYen;
    }
 
    const fromYenToPound = function(valueInYenes){
- 
     let valueInPound = valueInYenes * 0.8;
- 
     return valueInPound;
    }
 
